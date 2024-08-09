9a6acbcea5bcad4e7a4cb9d191d416a49698abb0
ibrahim-elshorbagy <e.mohmed55@gmail.com>
2024-08-09 22:54:00 +0200

    Commit to main-20240809-225314

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,7 +40,7 @@ class HandleInertiaRequests extends Middleware
                         'name' => $user->name,
                         'email' => $user->email,
                         'roles' => $user->getRoleNames(),
-                        'profile_photo_url'=> $user->profile_photo_url
+                        'profile_photo_url'=> $user->profile_photo_url ?? 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
                     ] : null;
                 },
                 ],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,43 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 /** @type {import('tailwindcss').Config} */
-export default {
-    darkMode: "class",
-
+module.exports = {
+    darkMode: ["class"],
     content: [
+        "./pages/**/*.{js,jsx}",
+        "./components/**/*.{js,jsx}",
+        "./app/**/*.{js,jsx}",
+        "./src/**/*.{js,jsx}",
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./resources/js/**/*.jsx",
     ],
-
+    prefix: "",
     theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
         extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
         },
     },
-
-    plugins: [forms],
+    plugins: [require("tailwindcss-animate"), forms],
 };
